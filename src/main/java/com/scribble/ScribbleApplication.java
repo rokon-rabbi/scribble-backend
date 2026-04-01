a0a66b1eac5c61cf7223bc0f94b63acfe53d8caa
Rokon <rokonrabbi21@gmail.com>
2026-04-02 00:02:22 +0600

    initialize project

---
+++ src/main/java/com/scribble/ScribbleApplication.java
@@ -0,0 +1,12 @@
+package com.scribble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScribbleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/scribble/ScribbleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.scribble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ScribbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
